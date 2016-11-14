cef3d8835664ff9fbd862069d3b65badd1c8c263
Ben Coppock <ben@goodpixelmedia.com>
2016-11-14 12:30:28 -0800

    Allow manual logging via Bunyan

--- lib/bunyan.ex
+++ lib/bunyan.ex
@@ -1,2 +1,45 @@
 defmodule Bunyan do
+  @moduledoc """
+  A JSON logger.
+
+  This module wraps the standard
+  [Elixir Logger](http://elixir-lang.org/docs/stable/logger/Logger.html)
+  and enables log messages to be generated in JSON format.
+
+  If used after Plug.RequestId, the output will contain the connection's
+  request_id.
+  """
+
+  require Logger
+
+  @spec info(binary, list) :: atom
+  def info(message, _opts \\ []) do
+    Logger.info fn ->
+      %{
+        "level"       => "info",
+        "timestamp"   => :os.timestamp |> format_timestamp |> List.to_string,
+        "logger_name" => "Bunyan",
+        "message"     => message
+      }
+      |> merge_request_id(Logger.metadata[:request_id])
+      |> Poison.encode!
+    end
+  end
+
+  @spec merge_request_id(map, binary) :: map
+  defp merge_request_id(log, nil), do: log
+
+  defp merge_request_id(log, request_id) do
+    Map.merge(log, %{"request_id" => request_id})
+  end
+
+  @spec format_timestamp({non_neg_integer, non_neg_integer, non_neg_integer}) :: list
+  defp format_timestamp({_,_,micro} = t) do
+    {{year,month,day},{hour,minute,second}} = :calendar.now_to_universal_time(t)
+
+    :io_lib.format(
+      "~4w-~2..0w-~2..0w ~2..0w:~2..0w:~2..0w.~6..0w",
+      [year,month,day,hour,minute,second,micro]
+    )
+  end
 end
--- test/bunyan_test.exs
+++ test/bunyan_test.exs
@@ -1,8 +1,68 @@
 defmodule BunyanTest do
-  use ExUnit.Case
-  doctest Bunyan
+  # From the io_capture docs: "when capturing something other than
+  # :stdio, the test should run with async false"
+  use ExUnit.Case, async: false
+  use Plug.Test
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import ExUnit.CaptureIO
+  require Logger
+
+  @timestamp_format ~r/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}/
+
+  defp log_with_bunyan(message, opts \\ []) do
+    data = capture_io(:user, fn ->
+      Bunyan.info(message, opts)
+      Logger.flush
+    end)
+
+    remove_extra_characters(data)
+  end
+
+  defmodule TestPlug do
+    use Plug.Builder
+    plug Plug.RequestId
+  end
+
+  defp log_with_plug(conn, message) do
+    data = capture_io(:user, fn ->
+      TestPlug.call(conn, [])
+      Bunyan.info(message)
+      Logger.flush
+    end)
+
+    remove_extra_characters(data)
+  end
+
+  defp remove_extra_characters(message) do
+    message
+    |> String.trim_leading("\e[22m")
+    |> String.trim_trailing("\n\e[0m")
+  end
+
+  test "includes the user message and other basic info" do
+    log = log_with_bunyan("my message") |> Poison.decode!
+
+    assert log["level"] == "info"
+    assert log["logger_name"] == "Bunyan"
+    assert Regex.match? @timestamp_format, log["timestamp"]
+    assert log["message"] == "my message"
+    refute Map.has_key?(log, "request_id")
+  end
+
+  test "includes automatic request_id when used with Plug.RequestId" do
+    log = conn(:get, "/")
+    |> log_with_plug("yeppers!")
+    |> Poison.decode!
+
+    assert log["request_id"]
+  end
+
+  test "passes custom request_id through when used with Plug.RequestId" do
+    log = conn(:post, "/")
+    |> put_req_header("x-request-id", "abc123-valid-request-id")
+    |> log_with_plug("yeppers!")
+    |> Poison.decode!
+
+    assert log["request_id"] == "abc123-valid-request-id"
   end
 end
