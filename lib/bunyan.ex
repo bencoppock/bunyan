7b9cc203c43fadc07fd6ff3cfb06bf3ea4aa8f2f
Ben Coppock <ben@goodpixelmedia.com>
2016-11-25 10:55:51 -0800

    Replace Bunyan.log with functions for each level
    * debug
    * info
    * warn
    * error

--- lib/bunyan.ex
+++ lib/bunyan.ex
@@ -10,27 +10,85 @@ defmodule Bunyan do
   request_id.
   """
 
+  @type message :: IO.chardata | String.Chars.t
+
+  @levels [:error, :warn, :info, :debug]
+
   require Logger
 
   alias Bunyan.Timestamp
 
-  @spec info(binary, list) :: atom
-  def info(message, _opts \\ []) do
-    Logger.info fn ->
-      %{
-        "level"       => "info",
-        "timestamp"   => Timestamp.format_string(:os.timestamp),
-        "logger_name" => "Bunyan",
-        "message"     => message
-      }
-      |> merge_request_id(Logger.metadata[:request_id])
-      |> Poison.encode!
-    end
+  @doc """
+  Logs a debug message.
+
+  Returns the atom `:ok` or an `{:error, reason}` tuple.
+
+  ## Examples
+      Bunyan.debug "--> Here! <--"
+      Bunyan.debug fn -> "expensive to calculate debug" end
+  """
+  @spec debug(message | (() -> message), Keyword.t) :: :ok | {:error, :noproc} | {:error, term}
+  def debug(chardata_or_fn, metadata \\ []) when is_list(metadata) do
+    Logger.debug(json(:debug, chardata_or_fn, metadata), metadata)
   end
 
-  @spec merge_request_id(map, binary) :: map
-  defp merge_request_id(log, nil), do: log
+  @doc """
+  Logs info.
 
+  Returns the atom `:ok` or an `{:error, reason}` tuple.
+
+  ## Examples
+      Bunyan.info "It's Friday"
+      Bunyan.info fn -> "expensive to calculate info" end
+  """
+  @spec info(message | (() -> message), Keyword.t) :: :ok | {:error, :noproc} | {:error, term}
+  def info(chardata_or_fn, metadata \\ []) when is_list(metadata) do
+    Logger.info(json(:info, chardata_or_fn, metadata), metadata)
+  end
+
+  @doc """
+  Logs a warning message.
+
+  Returns the atom `:ok` or an `{:error, reason}` tuple.
+
+  ## Examples
+      Bunyan.warn "Watch out!"
+      Bunyan.warn fn -> "expensive to calculate warning" end
+  """
+  @spec warn(message | (() -> message), Keyword.t) :: :ok | {:error, :noproc} | {:error, term}
+  def warn(chardata_or_fn, metadata \\ []) when is_list(metadata) do
+    Logger.warn(json(:warn, chardata_or_fn, metadata), metadata)
+  end
+
+  @doc """
+  Logs an error message.
+
+  Returns the atom `:ok` or an `{:error, reason}` tuple.
+
+  ## Examples
+      Bunyan.error "Oh no!!"
+      Bunyan.error fn -> "expensive to calculate error" end
+  """
+  @spec error(message | (() -> message), Keyword.t) :: :ok | {:error, :noproc} | {:error, term}
+  def error(chardata_or_fn, metadata \\ []) when is_list(metadata) do
+    Logger.error(json(:error, chardata_or_fn, metadata), metadata)
+  end
+
+  defp json(level, chardata_or_fn, _metadata) when level in @levels do
+    %{
+      "level"       => level,
+      "timestamp"   => Timestamp.format_string(:os.timestamp),
+      "logger_name" => "Bunyan",
+      "message"     => chardata_or_fn |> to_message
+    }
+    |> merge_request_id(Logger.metadata[:request_id])
+    |> Poison.encode!
+  end
+
+  defp to_message(data) when is_function(data), do: data.()
+  defp to_message(data) when is_list(data) or is_binary(data), do: to_string(data)
+
+  defp merge_request_id(log, nil), do: log
   defp merge_request_id(log, request_id) do
     Map.merge(log, %{"request_id" => request_id})
   end
--- test/bunyan_test.exs
+++ test/bunyan_test.exs
@@ -9,24 +9,9 @@ defmodule BunyanTest do
 
   @timestamp_pattern ~r/\A\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\z/
 
-  defp log_with_bunyan(message, opts \\ []) do
+  defp log_with_bunyan(level, message, metadata \\ []) do
     data = capture_io(:user, fn ->
-      Bunyan.info(message, opts)
-      Logger.flush
-    end)
-
-    remove_extra_characters(data)
-  end
-
-  defmodule TestPlug do
-    use Plug.Builder
-    plug Plug.RequestId
-  end
-
-  defp log_with_plug(conn, message) do
-    data = capture_io(:user, fn ->
-      TestPlug.call(conn, [])
-      Bunyan.info(message)
+      apply(Bunyan, level, [message, metadata])
       Logger.flush
     end)
 
@@ -36,32 +21,56 @@ defmodule BunyanTest do
   defp remove_extra_characters(message) do
     message
     |> String.trim_leading("\e[22m")
+    |> String.trim_leading("\e[31m")
+    |> String.trim_leading("\e[33m")
+    |> String.trim_leading("\e[36m")
     |> String.trim_trailing("\n\e[0m")
   end
 
   test "includes the user message and other basic info" do
-    log = log_with_bunyan("my message") |> Poison.decode!
+    log = log_with_bunyan(:info, "my message") |> Poison.decode!
 
-    assert log["level"] == "info"
     assert log["logger_name"] == "Bunyan"
     assert Regex.match? @timestamp_pattern, log["timestamp"]
     assert log["message"] == "my message"
     refute Map.has_key?(log, "request_id")
   end
 
-  test "includes automatic request_id when used with Plug.RequestId" do
-    log = conn(:get, "/")
-    |> log_with_plug("yeppers!")
-    |> Poison.decode!
+  test "allows \"debug\" level" do
+    log = log_with_bunyan(:debug, "my message") |> Poison.decode!
+    assert log["level"] == "debug"
+  end
+
+  test "allows \"warn\" level" do
+    log = log_with_bunyan(:warn, "my message") |> Poison.decode!
+    assert log["level"] == "warn"
+  end
+
+  test "allows \"error\" level" do
+    log = log_with_bunyan(:error, "my message") |> Poison.decode!
+    assert log["level"] == "error"
+  end
+
+  defmodule TestPlug do
+    use Plug.Builder
+    plug Plug.RequestId
+  end
+
+  test "includes a request_id when used in a process with Plug.RequestId" do
+    conn(:get, "/")
+    |> TestPlug.call([])
+
+    log = log_with_bunyan(:info, "yeppers!") |> Poison.decode!
 
     assert log["request_id"]
   end
 
-  test "passes custom request_id through when used with Plug.RequestId" do
-    log = conn(:post, "/")
+  test "includes custom request_id when used in a process with Plug.RequestId" do
+    conn(:post, "/")
     |> put_req_header("x-request-id", "abc123-valid-request-id")
-    |> log_with_plug("yeppers!")
-    |> Poison.decode!
+    |> TestPlug.call([])
+
+    log = log_with_bunyan(:info, "yeppers!") |> Poison.decode!
 
     assert log["request_id"] == "abc123-valid-request-id"
   end
