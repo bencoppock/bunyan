92526c5c730c5e60da5e2972a6ccd5f63f4b208f
Ben Coppock <ben@goodpixelmedia.com>
2016-11-25 12:34:39 -0800

    Update config

--- config/dev.exs
+++ config/dev.exs
@@ -1,3 +1,11 @@
 use Mix.Config
 
-import_config "test.exs"
+config :logger,
+  compile_time_purge_level: :info
+
+# config :logger, :console,
+#   format: "$message\n"
+
+config :bunyan,
+  env_vars: [{"CUSTOM_ENV_VAR", "our_env_var"}],
+  filter_parameters: ["password", "ssn"]
--- config/test.exs
+++ config/test.exs
@@ -1,9 +1,10 @@
 use Mix.Config
 
+config :logger,
+  compile_time_purge_level: :debug
+
 config :logger, :console,
-  format: "$message\n",
-  level: :info,
-  metadata: [:request_id]
+  format: "$message\n"
 
 config :bunyan,
   env_vars: [{"CUSTOM_ENV_VAR", "our_env_var"}],
